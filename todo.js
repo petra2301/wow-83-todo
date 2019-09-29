53848e5e0402a777c76cc78e1b46ff341c619efd
petra2301 <hello@petragergely.dk>
2019-09-29 12:29:45 +0200

    basic index content done

--- todo.js
+++ todo.js
@@ -5,9 +5,11 @@ const startBtn = document.querySelector(".startBtn");
 startBtn.addEventListener("click", closeStartScreen);
 
 function closeStartScreen() {
-    const startScreen = document.querySelector(".startScreen")
+    const startScreen = document.querySelector(".startScreen");
     startScreen.classList.add("shrinkStartScreen");
     startScreen.addEventListener("animationend", ()=> {
         startScreen.classList.add("hide");
     })
+
+    document.querySelector(".gradientBottom").classList.remove("hide");
 }
