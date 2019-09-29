80c279df78e7d7d3fe91d338b6312132d72ed39a
petra2301 <hello@petragergely.dk>
2019-09-29 11:54:14 +0200

    startscreen working

---
+++ todo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const startBtn = document.querySelector(".startBtn");
+
+startBtn.addEventListener("click", closeStartScreen);
+
+function closeStartScreen() {
+    const startScreen = document.querySelector(".startScreen")
+    startScreen.classList.add("shrinkStartScreen");
+    startScreen.addEventListener("animationend", ()=> {
+        startScreen.classList.add("hide");
+    })
+}
