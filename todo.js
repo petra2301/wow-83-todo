ec1ae7d9009df03dfe032bc9fde468f1c2d79088
petra2301 <hello@petragergely.dk>
2020-02-12 11:29:55 +0100

    tasks work for one char

--- todo.js
+++ todo.js
@@ -3,36 +3,10 @@
 //link https://todolist2019-e565.restdb.io/rest/addtaskform
 
 const startBtn = document.querySelector(".startBtn");
-const addForm = document.querySelector("form#addForm");
+const addForm = document.querySelector("#addForm");
 const addTaskBtn = document.querySelector(".addTaskBtn");
 const editForm = document.querySelector("form#editForm");
 const modal = document.querySelector(".modal");
-
-startBtn.addEventListener("click", closeStartScreen);
-
-addForm.addEventListener("submit", e => {
-    e.preventDefault();
-    addTaskBtn.textContent = "Waiting..."
-    post();
-  });
-
-editForm.addEventListener("submit", e => {
-    e.preventDefault();
-    put();
-    document.querySelector(".modal").classList.add("hide");
- })
-
-modal.querySelector(".closeModal").addEventListener("click", () => {
-    modal.classList.add("hide");
-    document.querySelector(".taskOgName").textContent = "";
-    editForm.reset();
-})
-
-modal.querySelector(".cancelEditing").addEventListener("click", () => {
-    modal.classList.add("hide");
-    document.querySelector(".taskOgName").textContent = "";
-    editForm.reset();
-})
 
 get();
 
@@ -132,7 +106,7 @@ function put() {
 .then( updatedTask => {
     const parentElement = document.querySelector(`article[data-task-id="${updatedTask._id}"]`);
     
-    parentElement.querySelector("h2").textContent = updatedTask.task;
+    parentElement.querySelector("h3").textContent = updatedTask.task;
     parentElement.querySelector(".taskDate").textContent = updatedTask.when;
     parentElement.querySelector(".taskNotes").textContent = updatedTask.notes;
 });
@@ -165,17 +139,7 @@ function addTask(task) {
     const clone = template.cloneNode(true);
   
     clone.querySelector("article").dataset.taskId = task._id;
-    clone.querySelector("h2").textContent = task.task;
-    clone.querySelector(".taskDate").textContent = task.when;
-    clone.querySelector(".taskNotes").textContent = task.notes;
-  
-    clone.querySelector("button.deleteBtn").addEventListener("click", () => {
-      deleteTaskAnimation(task._id);
-    });
-  
-    clone.querySelector("button.editBtn").addEventListener("click", e => {
-        editTask(task._id);
-    });
+    clone.querySelector("h3").textContent = task.task;
 
     clone.querySelector("button.doneBtn").addEventListener("click", () => {
         makeDone(task._id);
@@ -194,20 +158,6 @@ function showToast() {
     setTimeout(function(){
         toast.classList.remove("taskAdded");
     }, 4000);
-}
-
-function closeStartScreen() {
-    const startScreen = document.querySelector(".startScreen");
-    startScreen.querySelector("h1").classList.add("shrinkHeader");
-    startScreen.querySelector("p").classList.add("shrinkText");
-    startScreen.classList.add("shrinkStartScreen");
-    startScreen.addEventListener("animationend", ()=> {
-        startScreen.classList.add("hide");
-        document.querySelector("main").classList.remove("hide");
-        document.querySelector(".gradientBottom").classList.remove("hide");
-        document.querySelector("#yourTasks").classList.remove("hide");
-        document.querySelector(".header").classList.remove("hide");
-    })
 }
 
 function makeDone(id) {
